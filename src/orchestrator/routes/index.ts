d135e34fcfbeda100a50a8846c24ec961f091ee0
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2026-05-07 13:00:00 -0700

    refactor(routes): consolidate route exports into barrel index

--- src/orchestrator/routes/index.ts
+++ src/orchestrator/routes/index.ts
@@ -1,3 +1,6 @@
 export { createCampaignRouter } from './campaign.routes';
 export { createHealthRouter } from './health.routes';
-export { createApiRouter } from './api.routes';
+export { createStatusRouter as createApiRouter } from './status';
+export { createProspectsRouter } from './prospects';
+export { createDraftsRouter } from './drafts';
+export { createCampaignStatsRouter } from './campaigns';
