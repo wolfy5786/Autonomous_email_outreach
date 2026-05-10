0a1205333fd0a574a0d09de1e3c4c21aaf0afe19
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2026-05-10 14:30:00 -0700

    refactor(postgres): update barrel exports with all repositories

--- src/orchestrator/postgres/index.ts
+++ src/orchestrator/postgres/index.ts
@@ -1,2 +1,4 @@
 export { PostgresClient } from './client';
 export { CampaignStatusRepository } from './campaign-status.repo';
+export { CampaignRepository } from './campaignRepo';
+export { AuditLog } from './auditLog';
